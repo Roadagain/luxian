0dc9b1ad9228e61e292a29337668ff4b1f41f72d
Roadagain <ringoh72@gmail.com>
2019-08-31 14:26:59 +0900

    Reduxとおさらばする

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux'
-import Luxian from './components/Luxian';
-import lever from './reducers/lever';
+import Luxian from './Luxian';
 
-const store = createStore(lever);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Luxian />
-  </Provider>,
+  <Luxian />,
   document.getElementById('app')
 );
